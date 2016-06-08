d1be2485aba14858469ed2683397a934c764e5dc
Josh Williams <vmizzle@gmail.com>
2016-06-08 11:00:48 -0400

    First attempt at line parsing.

--- csv_grouper.rb
+++ csv_grouper.rb
@@ -17,4 +17,30 @@ class CSVGrouper
       end
     end
   end
+
+  def matching_columns(strings)
+    strings = [*strings]
+    headers.select do |h|
+      match = false
+      strings.each do |s|
+        if h.start_with?(s)
+          match = true
+          break
+        end
+      end
+      match
+    end
+  end
+
+  def parse_line(line)
+    values = line.split(',').collect { |s| s.strip }
+
+    # if the number of columns doesn't match, bail
+    return nil if values.length != headers.length
+
+    headers.each_with_index.inject({}) do |memo, (header, idx)|
+      memo[header] = values[idx]
+      memo
+    end
+  end
 end
--- spec/csv_grouper_spec.rb
+++ spec/csv_grouper_spec.rb
@@ -23,4 +23,33 @@ describe CSVGrouper do
       g.headers
     end
   end
+
+  describe 'matching_columns' do
+    it 'returns all columns that match the given criteria' do
+      g = CSVGrouper.new('test.csv')
+      allow(g).to receive(:headers).and_return(['phone1', 'email1', 'email2'])
+      expect(g.matching_columns('email')).to eql(['email1', 'email2'])
+    end
+
+    it 'accepts multiple names' do
+      g = CSVGrouper.new('test.csv')
+      allow(g).to receive(:headers).and_return(['last_name', 'phone1', 'email1', 'email2', 'zip'])
+      expect(g.matching_columns(['phone', 'email'])).to eql(['phone1', 'email1', 'email2'])
+    end
+  end
+
+  describe 'parse_line' do
+    let(:line) { 'Wilson, (123) 456-7890, foo@bar.com, biz@baz.com, 12345' }
+    it 'returns a hash representing the data in the line' do
+      g = CSVGrouper.new
+      allow(g).to receive(:headers).and_return(['last_name', 'phone1', 'email1', 'email2', 'zip'])
+      expect(g.parse_line(line)).to include({
+        'last_name' => 'Wilson',
+        'phone1' => '(123) 456-7890',
+        'email1' => 'foo@bar.com',
+        'email2' => 'biz@baz.com',
+        'zip' => '12345'
+      })
+    end
+  end
 end
