ec13b1764d64726cfe44abd0565083961658ae20
Josh Williams <vmizzle@gmail.com>
2016-06-08 16:22:03 -0400

    Refactored Grouper to use the new Store.

--- grouper.rb
+++ grouper.rb
@@ -1,63 +1,38 @@
 require 'csv'
+require 'store'
 require 'active_support/core_ext/object'
 
 class Grouper
 
   class InvalidFileError < StandardError; end
 
-  def initialize(filename = '')
+  def initialize(filename = '', match_fields = [])
     @filename = filename
-  end
+    @match_fields = [*match_fields]
 
-  def valid?
-    return false if @filename.blank?
-    return false unless File.exists?(@filename)
-    true
-  end
-
-  def process(match_fields = [])
     # Ensure the file we're processing is actually valid
-    raise InvalidFileError, "Invalid input file: #{@filename}" unless valid?
+    raise InvalidFileError, "input file required" if filename.blank?
+    raise InvalidFileError, "Invalid input file: #{@filename}" unless File.exists?(filename)
 
     # Ensure we were actually given at least 1 field to match
     match_fields = [*match_fields]
     raise ArgumentError, "No match fields given" if match_fields.empty?
 
+    @store = Store.new(match_fields)
+  end
+
+  def process(match_fields = [])
     f = File.open(@filename, "r")
     csv = CSV.new(@filename, headers: true)
-    headers = nil
+    headers_printed = false
     csv.each do |row|
-      if headers.nil?
-        headers = csv.headers
-        create_lookup_tables(match_fields, csv.headers)
-        output "Id,#{csv.headers.join(',')}"
+      unless headers_printed
+        output "Id,#{row.headers.join(',')}"
+        headers_printed = true
       end
-      id = lookup_id(row)
+      id = @store.put(row)
       output "#{id},#{row.to_csv}"
     end
     f.close
-  end
-
-  def create_lookup_tables(match_fields, headers)
-    # these hashes have the potential to become very large.  If processing
-    # extremely large files, these should be moved to ACTUAL tables in a
-    # sqlite (or similar) external database.
-
-    # first, extract the actual keys from the match_fields
-    keys = match_fields.collect { |s| s.split('_', 2).last.underscore }
-
-    # now, run the headers through the same transformation
-    @lookup_tables = {}
-    headers.each do |h|
-      keys.each do |key|
-        if h.underscore.start_with?(key)
-          @lookup_tables[h] = {}
-        end
-      end
-    end
-    @lookup_tables
-  end
-
-  def lookup_id(row)
   end
 end
--- spec/grouper_spec.rb
+++ spec/grouper_spec.rb
@@ -3,147 +3,71 @@ require 'grouper'
 
 describe Grouper do
 
-  it { is_expected.to respond_to(:valid?) }
+  before do
+    allow(File).to receive(:exists?).and_return(true)
+  end
 
-  describe '#valid?' do
-    it 'ensures a filename was passed' do
-      g = Grouper.new
-      expect(g.valid?).to be false
-    end
+  it 'requires a valid input file' do
+    allow(File).to receive(:exists?).and_return(false)
+    expect { Grouper.new('invalid.csv') }.to raise_error(Grouper::InvalidFileError)
+  end
 
-    it 'ensures the file exists' do
-      g = Grouper.new('invalid.csv')
-      expect(File).to receive(:exists?).with('invalid.csv').and_return(false)
-      expect(g.valid?).to be false
-    end
+  it 'requires at least 1 match_field' do
+    allow(File).to receive(:exists?).and_return(true)
+    expect { Grouper.new('valid.csv', nil) }.to raise_error(ArgumentError)
   end
 
   describe '#process' do
-    let(:grouper) { Grouper.new('test.csv') }
+    let(:grouper) { Grouper.new('test.csv', %w(email)) }
+    let(:headers) { %w(RowHeaders) }
+    let(:row1) { double('CSV::Row', headers: %w(Row1Headers), to_csv: 'Row1Data') }
+    let(:row2) { double('CSV::Row', headers: %w(Row2Headers), to_csv: 'Row2Data') }
+    let(:row3) { double('CSV::Row', headers: %w(Row3Headers), to_csv: 'Row3Data') }
+    let(:csv) { double('CSV') }
+    let(:file) { double('File', close: nil) }
+    let(:store) { double('Store', put: '1111') }
 
-    context 'with a non-existent file' do
-      before do
-        allow(grouper).to receive(:valid?).and_return(false)
-      end
-
-      it 'does not open the CSV file' do
-        expect(File).not_to receive(:open)
-        begin
-          grouper.process(nil)
-        rescue; end
-      end
-
-      it 'does not create a CSV object' do
-        expect(CSV).not_to receive(:new)
-        begin
-          grouper.process(nil)
-        rescue; end
-      end
-
-      it 'raises an InvalidFileError' do
-        expect { grouper.process(nil) }.to raise_error(Grouper::InvalidFileError)
-      end
+    before do
+      allow(Store).to receive(:new).and_return(store)
+      allow(grouper).to receive(:output)
+      allow(File).to receive(:open).and_return(file)
+      allow(CSV).to receive(:new).and_return(csv)
+      allow(csv).to receive(:each).and_yield(row1).and_yield(row2).and_yield(row3)
     end
 
-    context 'with a valid file' do
-
-      before do
-        allow(grouper).to receive(:valid?).and_return(true)
-      end
-
-      context 'and no match_fields supplied' do
-        let(:match_fields) { nil }
-
-        it 'does not open the CSV file' do
-          expect(File).not_to receive(:open)
-          begin
-            grouper.process(match_fields)
-          rescue; end
-        end
-
-        it 'does not create a CSV object' do
-          expect(CSV).not_to receive(:new)
-          begin
-            grouper.process(match_fields)
-          rescue; end
-        end
-
-        it 'raises ArgumentError' do
-          expect { grouper.process(match_fields) }.to raise_error(ArgumentError)
-        end
-      end
-
-      context 'and a list of match_fields supplied' do
-
-        let(:match_fields) { ['same_email'] }
-
-        let(:row1) { double('CSV::Row', to_csv: 'Row1') }
-        let(:row2) { double('CSV::Row', to_csv: 'Row2') }
-        let(:row3) { double('CSV::Row', to_csv: 'Row3') }
-        let(:csv) { double('CSV', headers: %w(FirstName LastName Email1 Email2)) }
-        let(:file) { double('File', close: nil) }
-
-        before do
-          allow(grouper).to receive(:valid?).and_return(true)
-          allow(grouper).to receive(:output)
-          allow(File).to receive(:open).and_return(file)
-          allow(CSV).to receive(:new).and_return(csv)
-          allow(csv).to receive(:each).and_yield(row1).and_yield(row2).and_yield(row3)
-        end
-
-        it 'opens the CSV file for reading' do
-          expect(File).to receive(:open).and_return(file)
-          grouper.process(match_fields)
-        end
-
-        it 'creates a CSV object' do
-          expect(CSV).to receive(:new)
-          grouper.process(match_fields)
-        end
-
-        it 'creates the lookup tables' do
-          expect(grouper).to receive(:create_lookup_tables).with(match_fields, csv.headers).once
-          grouper.process(match_fields)
-        end
-
-        it 'prints the headers' do
-          expect(grouper).to receive(:output).with(
-            'Id,FirstName,LastName,Email1,Email2'
-          )
-          grouper.process(match_fields)
-        end
-
-        it 'looks up the Id for each row' do
-          expect(grouper).to receive(:lookup_id).exactly(3).times
-          grouper.process(match_fields)
-        end
-
-        it 'prints each row, including the Id' do
-          allow(grouper).to receive(:lookup_id).and_return('1')
-          expect(grouper).to receive(:output).with('1,Row1')
-          expect(grouper).to receive(:output).with('1,Row2')
-          expect(grouper).to receive(:output).with('1,Row3')
-          grouper.process(match_fields)
-        end
-
-        it 'closes the CSV file' do
-          expect(file).to receive(:close)
-          grouper.process(match_fields)
-        end
-      end
+    it 'opens the CSV file for reading' do
+      expect(File).to receive(:open).and_return(file)
+      grouper.process
     end
-  end
 
-  describe '#create_lookup_tables' do
-    let(:match_fields) { ['same_email', 'same_last_name'] }
-    let(:headers) { %w(FirstName LastName Email1 Email2 Phone1 Phone2) }
+    it 'creates a CSV object' do
+      expect(CSV).to receive(:new)
+      grouper.process
+    end
 
-    it 'finds all matching columns and creates appropriate tables' do
-      grouper = Grouper.new
-      tables = grouper.create_lookup_tables(match_fields, headers)
-      expect(tables).to have_key('LastName')
-      expect(tables).to have_key('Email1')
-      expect(tables).to have_key('Email2')
+    it 'prints the headers' do
+      expect(grouper).to receive(:output).with(
+        'Id,Row1Headers'
+      )
+      grouper.process
+    end
+
+    it 'looks up the Id for each row' do
+      expect(store).to receive(:put).exactly(3).times
+      grouper.process
+    end
+
+    it 'prints each row, including the Id' do
+      allow(grouper).to receive(:store_row).and_return('1')
+      expect(grouper).to receive(:output).with('1111,Row1Data')
+      expect(grouper).to receive(:output).with('1111,Row2Data')
+      expect(grouper).to receive(:output).with('1111,Row3Data')
+      grouper.process
+    end
+
+    it 'closes the CSV file' do
+      expect(file).to receive(:close)
+      grouper.process
     end
   end
 end
